7a0785020781206e097978288a9ce43080b629fd
juliannguyen1 <jndnguyen@ucdavis.edu>
2019-12-18 10:25:40 -0800

    wip

--- src/main/java/best_route_mod/BestRouteMod.java
+++ src/main/java/best_route_mod/BestRouteMod.java
@@ -10,6 +10,7 @@ import com.megacrit.cardcrawl.map.MapRoomNode;
 import com.megacrit.cardcrawl.rooms.AbstractRoom;
 
 import java.util.ArrayList;
+import java.util.Map;
 
 @SpireInitializer
 public class BestRouteMod implements basemod.interfaces.PostUpdateSubscriber, basemod.interfaces.StartActSubscriber  {
@@ -19,9 +20,6 @@ public class BestRouteMod implements basemod.interfaces.PostUpdateSubscriber, ba
     public BestRouteMod() {
         BaseMod.subscribe(this);
         System.out.println("Best Route Mod initialized. Enjoy! -Mysterio's Magical Assistant");
-
-        // Initialize variables
-        bestPath = new ArrayList<MapRoomNode>();
     }
 
     public static void initialize() {
@@ -34,9 +32,21 @@ public class BestRouteMod implements basemod.interfaces.PostUpdateSubscriber, ba
     public void receivePostUpdate() {
         if(AbstractDungeon.currMapNode != null && !printedMap) {
             ArrayList<MapRoomNode> startingNodes = AbstractDungeon.map.get(1);
-
+            ArrayList<MapRoomNode> currentPath;
+            int highestRestSiteCount = 0;
             for(MapRoomNode startingNode: startingNodes){
-                findBestPathRecur(startingNode);
+                if(bestPath == null){
+                    bestPath = traversePathRecur(startingNode);
+                    highestRestSiteCount = getNumberOfCampSites(bestPath);
+                }else{
+                    currentPath = traversePathRecur(startingNode);
+                    // Compare number of rest sites
+                    int numRestSitesForCurrentPath = getNumberOfCampSites(currentPath);
+                    if(numRestSitesForCurrentPath > highestRestSiteCount){
+                        highestRestSiteCount = numRestSitesForCurrentPath;
+                        bestPath = currentPath;
+                    }
+                }
             }
 
             //System.out.println("Num of edges: " + AbstractDungeon.currMapNode.getEdges().size());
@@ -45,18 +55,30 @@ public class BestRouteMod implements basemod.interfaces.PostUpdateSubscriber, ba
         }
     }
 
-    private ArrayList<MapRoomNode> findBestPathRecur(MapRoomNode node){
-
+    private ArrayList<MapRoomNode> traversePathRecur(MapRoomNode node){
+        ArrayList<MapRoomNode> connectedTopNodes = getConnectedTopNodes(node);
+        if(connectedTopNodes.isEmpty()){
+            return new ArrayList<MapRoomNode>();
+        }
+        for(MapRoomNode topNode: connectedTopNodes){
+            traversePathRecur(topNode);
+        }
     }
 
     // TODO: make sure it only gets the edges moving upwards
-    private ArrayList<MapRoomNode> getConnectedNodesOnTop(MapRoomNode node){
+    private ArrayList<MapRoomNode> getConnectedTopNodes(MapRoomNode node){
         ArrayList<MapEdge> mapEdges = node.getEdges();
         mapEdges.forEach(mapEdge -> printEdge(mapEdge));
     }
 
     private MapRoomNode getNodeAtCoordinates(int x, int y){
         return AbstractDungeon.map.get(y+1).get(x);
+    }
+
+    // TODO: filter out all the nodes except for rest sites and count them
+    private int getNumberOfCampSites(ArrayList<MapRoomNode> path){
+        path.removeIf(mapRoomNode -> mapRoomNode.room.combatEvent);
+        return path.size();
     }
 
     // Debug functions
